c71277dbc1a1f9d1409ae933369d6158cbb3ff30
giuliagolesano <giulia.golesano@studio.unibo.it>
2025-01-18 12:47:37 +0100

    form login okay, prodocts i'm not sure

---
+++ template/products.php
@@ -0,0 +1,12 @@
+<?php if(isset($templateParams["titolo_pagina"])): ?>
+    <h2><?php echo $templateParams["titolo_pagina"]; ?></h2>
+<?php endif; ?>
+<?php foreach($templateParams["prodotti"] as $prodotto): ?>
+    <div>
+        <h3><?php echo $prodotto["nome"]; ?></h3>
+        <p><?php echo $prodotto["descrizione"]; ?></p>
+        <p><?php echo $prodotto["prezzo"]; ?></p>
+        <p><?php echo $prodotto["quantita"]; ?></p>
+        <img src="<?php echo UPLOAD_DIR.$prodotto["immagine"]; ?>" alt="<?php echo $prodotto["nome"]; ?>">
+    </div>
+<?php endforeach; ?>
