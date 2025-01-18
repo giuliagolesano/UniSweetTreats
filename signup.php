fa8daf819aa58bcecc5a47f632de37ff723b1618
giuliagolesano <giulia.golesano@studio.unibo.it>
2025-01-18 13:23:22 +0100

    sign up calls the query signUpUser

--- signup.php
+++ signup.php
@@ -4,5 +4,23 @@ require_once("bootstrap.php");
 $templateParams["titolo"] = "Uni Sweet Treats - Sign up";
 $templateParams["nome"] = "signup-form.php";
 
+if(isset($_POST["submit"])) {
+    $email = $_POST["email"];
+    $passworn = $_POST["password"];
+    $name = $_POST["name"];
+    $surname = $_POST["surname"];
+    $consenso = $_POST["consenso"];
+
+    $registration = $db->signUpUser($email, $name, $surname, $password);
+    if($registration) {
+        header("location: login.php");
+        exit;
+    }else {
+        die("Errore nella registrazione");
+        exit;
+    }
+    exit;
+}
+
 require 'template/base.php';
 ?>
--- to-do.md
+++ to-do.md
@@ -1,3 +1,6 @@
 **Cosa manca?**
 - query per estrarre 3 foto di prodotti casuali (con numero di foto passato come parametro perché ce ne servono 3 all'inizio della home e circa 8/9 alla fine per decorazione)
 - query per iscrivere la email alla newsletter che cerca l'utente 
+- nella query signUpUser aggiungere il consenso tra i parametri
+- controlliamo che esista già l'utente che cerca di registrarsi? in quel caso aggiungere query che cerca una determinata email nel db 
+- 
