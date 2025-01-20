0cd060445652eb15ce9ce9ebb82e80786a7051b1
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2025-01-20 15:11:28 +0100

    fixed single product php file

--- product.php
+++ product.php
@@ -7,13 +7,11 @@ if(!(isset($_GET["id_prod"]))) {
 
 $templateParams["titolo"] = "Uni Sweet Treats - Product";
 $templateParams["nome"] = "single-product.php";
-$templateParams["js"][] = "js/product.js"
+$templateParams["js"][] = "js/product.js";
 
 $userParams["product"] = current(array_filter($userParams["products"], function($product) {
     return $product["id_prod"] == $_REQUEST["id_prod"];
-}))
+}));
 
 require("template/base.php");
 ?>
-
-
--- template/single-product.php
+++ template/single-product.php
@@ -1,22 +1,39 @@
-<?php if(count($templateParams["prodotti"])== 0): ?>
+<?php if(empty($userParams["product"])): ?>
     <h2>Non ci sono prodotti</h2>
-<?php 
-    else:
-        $prodotto = $templateParams["prodotti"][0];
+<?php else:
+        $prodotto = $userParams["product"];
 ?>
     <div class="product">
         <div class="product-image">
-            <img src="<?php echo UPLOAD_DIR.$prodotto["immagine"]; ?>" alt="<?php echo $prodotto["nome"]; ?>">
+            <img src="<?php echo getImageProduct($prodotto["nome_categoria"], $prodotto["immagine"]); ?>" alt="<?php echo $prodotto["nome"]; ?>">
         </div>
         <div class="product-info">
             <h2><?php echo $prodotto["nome"]; ?></h2>
             <p><?php echo $prodotto["descrizione"]; ?></p>
-            <p><?php echo $prodotto["prezzo"]; ?></p>
-            <p><?php echo $prodotto["quantita"]; ?></p>
-            <form action="#" method="POST">
-                <input type="hidden" name="id" value="<?php echo $prodotto["id"]; ?>">
+            <p>Prezzo: €<?php echo $prodotto["prezzo"]; ?></p>
+            <p>Quantità disponibile: <?php echo $prodotto["quantita"]; ?></p>
+            <form action="cart.php" method="POST" onchange="updateQuantity()">
                 <input type="number" name="quantita" value="1" min="1" max="<?php echo $prodotto["quantita"]; ?>">
-                <input type="submit" name="aggiungi" value="Aggiungi al carrello">
+                <?php if($prodotto["nome_categoria"] == "cake"): ?>
+                    <div>
+                        <label for="custom-text">Customize your cake:</label>
+                        <input type="text" id="custom-text" name="custom-text" placeholder="Write your text here..." />
+                    </div>
+                    <div>
+                        <label for="upload-photo">Upload a photo:</label>
+                        <input type="file" id="upload-photo" name="upload-photo" />
+                    </div>
+                    <fieldset>
+                        <legend>Choose your topping:</legend>
+                        <input type="radio" id="sprinkles" name="topping" value="sprinkles" />
+                        <label for="sprinkles">Sprinkles</label><br/>
+                        <input type="radio" id="edible-glitters" name="topping" value="edible-glitters" />
+                        <label for="edible-glitters">Edible glitters</label><br/>
+                        <input type="radio" id="none" name="topping" value="none" />
+                        <label for="none">None</label><br/>
+                    </fieldset>
+                <?php endif; ?>
+                <input type="submit" name="aggiungi" value="Aggiungi al carrello" />
             </form>
         </div>
     </div>
