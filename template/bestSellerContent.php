d60690119d6c84c50662ad42de28e496cfc97fd1
giuliagolesano <giulia.golesano@studio.unibo.it>
2025-01-21 14:49:33 +0100

    best seller css

--- template/bestSellerContent.php
+++ template/bestSellerContent.php
@@ -1,10 +1,12 @@
 <h3>BEST SELLERS</h3>
-<div>
-    <?php foreach($templateParams["bestSellers"] as $bestSeller): ?>
-        <div>
-            <h4><?php echo strtoupper($bestSeller["nomeTip"]); ?></h4>
-            <img src="<?php echo getImageProduct($bestSeller["nomeTip"], $bestSeller["foto"]); ?>" alt="<?php echo $bestSeller["descrizione"]; ?> Image">
-            <h5><?php echo $bestSeller["descrizione"]; ?></h5>
-        </div>
-    <?php endforeach; ?>
+<div class="bestSeller">
+    <div>
+        <?php foreach($templateParams["bestSellers"] as $bestSeller): ?>
+            <div>
+                <h4><?php echo strtoupper($bestSeller["nomeTip"]); ?></h4>
+                <img src="<?php echo getImageProduct($bestSeller["nomeTip"], $bestSeller["foto"]); ?>" alt="<?php echo $bestSeller["descrizione"]; ?> Image">
+                <h5><?php echo $bestSeller["descrizione"]; ?></h5>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </div>
