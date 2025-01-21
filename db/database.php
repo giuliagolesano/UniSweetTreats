da63a7e93a8c4bce036ddf386e34bba0f7c7095a
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2025-01-21 16:58:15 +0100

    updated some database function

--- db/database.php
+++ db/database.php
@@ -255,7 +255,7 @@ class DatabaseHelper{
     }
 
     public function getOrderCart($email) {
-        $stmt = $this->db->prepare("SELECT * FROM ORDINE WHERE stato = 'attesa' AND e_mail = ?");
+        $stmt = $this->db->prepare("SELECT * FROM ORDINE WHERE stato = 'waiting' AND e_mail = ?");
         $stmt->bind_param('s', $email);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -267,16 +267,16 @@ class DatabaseHelper{
         $row = $result->fetch_assoc(); //COULD CHECK IF THE QUERY RETURNED A ROW EMPTY
         $newCodOrd = 'ord' . ((int)substr($row['maxCodOrd'], 3) + 1); // create a new order code increasing the last one
         $stmt = $this->db->prepare("INSERT INTO ORDINE (codOrd, giorno, ora, stato, e_mail) 
-                                    VALUES (?, CURDATE(), CURTIME(), 'attesa', ?)");
+                                    VALUES (?, CURDATE(), CURTIME(), 'waiting', ?)");
         $stmt->bind_param('ss', $newCodOrd, $email);
         $stmt->execute();
         return $newCodOrd;
     }
 
     public function addProductToCart($codOrd, $codProd, $quantita, $customText, $photoName, $topping) {
-        $stmt = $this->db->prepare("INSERT INTO FORMATO_DA (codOrd, codProd, foto, testo, topping) 
-                                    VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param('sssss', $codOrd, $codProd, $photoName, $customText, $topping);
+        $stmt = $this->db->prepare("INSERT INTO FORMATO_DA (codOrd, codProd, foto, testo, topping, quantita) 
+                                    VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param('ssssss', $codOrd, $codProd, $photoName, $customText, $topping, $quantita);
         $stmt->execute();
     }
 
